767b449e002e0b060a4dd408739c8a81bfb08e2b
Ethan <elwebb1117@gmail.com>
2017-01-19 14:36:04 -0600

    Instead of the current Joystick code, I replaced it with essentially the same code, but now it is easier to edit. I also did SmartDashboard for the drivetrain and linked it to OI. I also edited Teleop slightly to compensate for the change in Joystick controls.

--- src/org/usfirst/frc/team5002/robot/OI.java
+++ src/org/usfirst/frc/team5002/robot/OI.java
@@ -3,19 +3,31 @@ package org.usfirst.frc.team5002.robot;
 import edu.wpi.first.wpilibj.Joystick;
 
 
+
 /**
+ * @author elweb @version Last Modified 1/18/17
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	public Joystick arcadeStick;
+	private Joystick arcadeStick; //named Joystick
 	
 	public OI(){
-		arcadeStick = new Joystick(0);
+		arcadeStick = new Joystick(0); //gave Joystick a job
 	}
 	
-	public Joystick getJoystick(){
-		return arcadeStick;
-		
+	public double getForwardAxis() {
+		return arcadeStick.getX();//allows the Joystick to command the Robot's forwards and backwards movement
+	}
+	
+	public double getHorizontalAxis(){
+		return arcadeStick.getY();//allows the Joystick to command the Robot's side to side movement
+	}
+	
+	public double getTurnAxis(){
+		return arcadeStick.getZ();//allows the Joystick to command the rotation of the Robot
+	}
+	public void UpdateSD(){
+		Robot.drivetrain.UpdateSD();//sends all the data from SwerveDrive subsystem to the SmartDashboard
 	}
 }
