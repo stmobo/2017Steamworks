c731799d2eb39e9b077c71a5bd157d6adbb81583
gracecapooth <gracecapooth@172.20.10.2>
2017-02-08 15:16:08 -0600

    Finished the Autonomous code with Sebastian, will add author and detailed comments later

---
+++ src/org/usfirst/frc/team5002/robot/commands/AutonomousFinal.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team5002.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team5002.robot.subsystems.SwerveDrive;
+import org.usfirst.frc.team5002.robot.subsystems.*;
+import org.usfirst.frc.team5002.robot.*;
+
+
+/**
+ *
+ */
+public class AutonomousFinal extends Command {
+
+    public AutonomousFinal() {
+    	
+    	requires(Robot.drivetrain);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	//variable x is equal to the distance in feet(x12) divded by 4
+    	//change the first number to the feet you want
+    	double x = (8*12)/12.56;
+    	
+    	//set all the motors in the drivetrain to the variable x, and itll run forward
+    	Robot.drivetrain.fl_drive.set(x);
+    	Robot.drivetrain.fr_drive.set(x);
+    	Robot.drivetrain.bl_drive.set(x);
+    	Robot.drivetrain.br_drive.set(x);
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
