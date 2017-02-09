8d96895fd3e06830b1c4b103ecd23931644514d4
Sebastian Mobo <stmobo@gmail.com>
2017-02-08 18:19:22 -0600

    Add button support for replay

--- src/org/usfirst/frc/team5002/robot/OI.java
+++ src/org/usfirst/frc/team5002/robot/OI.java
@@ -28,28 +28,42 @@ public class OI {
     public boolean currentlyReplaying = false;
     public int currentReplayIndex = 0;                                      // Our index into the state list in currentReplay (zero-based).
 
+    public ControlState lastState = ControlState.getDefaultInstance();      // Previous control state.
     public ControlState currentState = ControlState.getDefaultInstance();   // Current control state.
     public Replay currentReplay;                                            // The replay we're reading from. Will be null if not replaying (i.e. in teleop / no replay loaded).
     public Replay.Builder currentRecording;                                 // The builder we're writing to. Will be null if not recording.
 
+    private boolean toggleA = false;
+    private boolean toggleB = false;
+
+    /* Keep instances of the Commands here to share between teleop & replay-auto */
+    private ClimbUp climbup = new ClimbUp();
+    private ClimbDown climbdown = new ClimbDown();
+    private LaunchererC launchererC = new LaunchererC();
+    private INtaker intaker = new INtaker();
+    private OUTtaker outtaker = new OUTtaker();
+    private TakeOuter takeouter = new TakeOuter();
+    private ReverseInTaker reverseInTaker = new ReverseInTaker();
+
+    /* Ditto with the buttons. */
+    public Button A = new JoystickButton(arcadeStick, 1);
+    public Button B = new JoystickButton(arcadeStick, 2);
+    public Button X = new JoystickButton(arcadeStick, 3);
+    public Button Y = new JoystickButton(arcadeStick, 4);
+    public Button LB = new JoystickButton(arcadeStick, 5);
+    public Button RB = new JoystickButton(arcadeStick, 6);
+    public Button home = new JoystickButton(arcadeStick, 7);
+    public Button menu = new JoystickButton(arcadeStick, 8);
+
 	public OI(){
 		arcadeStick = new Joystick(0); //gave Joystick a job
-		Button A = new JoystickButton(arcadeStick, 1);
-		Button B = new JoystickButton(arcadeStick, 2);
-		Button X = new JoystickButton(arcadeStick, 3);
-		Button Y = new JoystickButton(arcadeStick, 4);
-		Button LB = new JoystickButton(arcadeStick, 5);
-		Button RB = new JoystickButton(arcadeStick, 6);
-		Button home = new JoystickButton(arcadeStick, 7);
-		Button menu = new JoystickButton(arcadeStick, 8);
-
-		Y.whileHeld(new ClimbUp());//turns the climb motor on while Y is being held
-		B.toggleWhenPressed(new LaunchererC());//turns launcher motor on when B is pressed once, and off when B is pressed again
-		A.toggleWhenPressed(new INtaker()); //turns the intake motor on when A is pressed once, and off when A is pressed again
-		A.toggleWhenPressed(new OUTtaker()); //turns the outake motor on at the same time as the intake motor
-		LB.whileHeld(new TakeOuter()); // emergency reverse for outtake motor
-		LB.whileHeld(new ReverseInTaker());// emergency reverse for intake motor
-		LB.whileHeld(new ClimbDown());//emergency reverse for climb motor
+		Y.whileHeld(climbup);//turns the climb motor on while Y is being held
+		B.toggleWhenPressed(launchererC);//turns launcher motor on when B is pressed once, and off when B is pressed again
+		A.toggleWhenPressed(intaker); //turns the intake motor on when A is pressed once, and off when A is pressed again
+		A.toggleWhenPressed(outtaker); //turns the outake motor on at the same time as the intake motor
+		LB.whileHeld(takeouter); // emergency reverse for outtake motor
+		LB.whileHeld(reverseInTaker);// emergency reverse for intake motor
+		LB.whileHeld(climbdown);//emergency reverse for climb motor
 	}
 
     private double getRawForwardAxis() {
@@ -66,6 +80,7 @@ public class OI {
 
     public void loadStateFromReplay() {
         if( currentReplay != null && currentReplayIndex < currentReplay.getStateCount()) {
+            lastState = currentState;
             currentState = currentReplay.getState(currentReplayIndex);
             currentReplayIndex += 1;
         } else {
@@ -74,7 +89,50 @@ public class OI {
             currentState = ControlState.getDefaultInstance();
         }
 
-        // TODO: If/when button-triggered commands are added, make sure to trigger them here if necessary.
+        /* Trigger button-based commands if necessary: */
+        if(currentState.getButtonY()) {
+            climbup.start();
+        } else {
+            climbup.cancel();
+        }
+
+        if(currentState.getButtonLB()) {
+            takeouter.start();
+            reverseInTaker.start();
+            climbdown.start();
+        } else {
+            takeouter.cancel();
+            reverseInTaker.cancel();
+            climbdown.cancel();
+        }
+
+        /* Keep track of toggle state. */
+
+        /* Check for button state transition:  */
+        /* If button A was not pressed last time and it is pressed now... */
+        if(!lastState.getButtonA() && currentState.getButtonA()) {
+            toggleA = !toggleA; // Invert toggleA.
+        }
+
+        /* Ditto for B. */
+        if(!lastState.getButtonB() && currentState.getButtonB()) {
+            toggleB = !toggleB;
+        }
+
+        /* Start toggled commands if necessary. */
+        if(toggleA) {
+            intaker.start();
+            outtaker.start();
+        } else {
+            intaker.cancel();
+            outtaker.cancel();
+        }
+
+        if(toggleB) {
+            launchererC.start();
+        } else {
+            launchererC.cancel();
+        }
     }
 
     public void saveStateToReplay() {
@@ -84,6 +142,14 @@ public class OI {
                     .setForwardAxis(getRawForwardAxis())
                     .setHorizontalAxis(getRawHorizontalAxis())
                     .setTurnAxis(getRawTurnAxis())
+                    .setButtonA(A.get())
+                    .setButtonB(B.get())
+                    .setButtonX(X.get())
+                    .setButtonY(Y.get())
+                    .setButtonLB(LB.get())
+                    .setButtonRB(RB.get())
+                    .setButtonHome(home.get())
+                    .setButtonMenu(menu.get())
             );
         }
     }
