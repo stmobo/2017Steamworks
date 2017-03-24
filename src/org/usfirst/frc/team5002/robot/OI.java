761ee94ff019f62e28614ed215b3b5780adc7533
Sebastian Mobo <stmobo@gmail.com>
2017-03-23 23:01:12 -0400

    asdfasdfasdfasdfasdfasdfadsf

--- src/org/usfirst/frc/team5002/robot/OI.java
+++ src/org/usfirst/frc/team5002/robot/OI.java
@@ -191,7 +191,9 @@ public class OI {
 
 	public void UpdateSD(){
 		Robot.drivetrain.updateSD();//sends all the data from SwerveDrive subsystem to the SmartDashboard
-		//Robot.viewport.updateSD();
+		if(Robot.viewport != null) {
+			Robot.viewport.updateSD();	
+		}
         Robot.sensors.updateSD();
 
 		if(!DriverStation.getInstance().isDisabled()) {
